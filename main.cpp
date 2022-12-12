28de1878821ca1bcac618ef7455b1e8dbe068af9
Benny7102 <113646460+Benny7102@users.noreply.github.com>
2022-12-11 19:22:17 -0500

    mc along with main

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+#include "mc.h"
+
+using namespace std;
+
--- mc.cpp
+++ mc.cpp
@@ -22,4 +22,72 @@ mc::mc(int h, int d)
     gold = 0;
 }
 
-mc::remove_health()
+void mc::remove_health(int d)
+{
+    health = health - d;
+}
+
+void mc::healing(int h)
+{
+    health = health + h;
+}
+
+void mc::add_damage(int d)
+    {
+        damage = damage + d;
+    }
+
+void mc::remove_damage(int d)
+{
+    damage = damage - d;
+}
+
+void mc::add_treasure()
+{
+    srand (time(NULL));
+    int random =1 + (rand() %3);
+    treasure = treasure + random;
+}
+
+void mc::remove_gold(int cost)
+{
+    gold = gold - cost;
+}
+
+void mc::add_gold()
+{
+    srand (time(NULL));
+    int random =1 + (rand() %21);
+    gold = gold + random;
+}
+
+int mc::returnwin()
+{
+    return treasure;
+}
+
+bool mc::killed()
+{
+    if (health < 0){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+int mc::attack()
+{
+    return damage;
+}
+
+
+void mc::showstatus()
+{
+    cout << "Player Status" << endl;
+    cout << "health: " << health << endl;
+    cout << "attack damage: " << damage << endl;
+    cout << "gold: " << gold << endl;
+    cout << "treasure: " << treasure << endl;
+}
+
