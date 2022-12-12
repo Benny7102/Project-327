942f6698527963adaeea18ff15102ad49885e9c8
kennethbyun <114690778+kennethbyun@users.noreply.github.com>
2022-12-11 21:13:18 -0500

    Add files via upload

---
+++ Bridge.h
@@ -0,0 +1,16 @@
+#ifndef Bridge_H
+#define Bridge_H
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "mc.h"
+
+class Bridge
+{
+    public:
+    int bridge_num;
+	BridgeCondition(mc* player, int, int);
+}
+
+#endif
