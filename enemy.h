fb13345a8b83233769279f8afa732419141ec4eb
Benny7102 <113646460+Benny7102@users.noreply.github.com>
2022-12-11 20:34:27 -0500

    enemy

--- enemy.h
+++ enemy.h
@@ -1,22 +1,24 @@
+#ifndef ENEMY_H
+#define ENEMY_H
 #include <iostream>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <time.h>
-using namespace std;
 #include "mc.h"
-#ifndef ENEMY_H
-#define ENEMY_H
+
 class enemy
 {
-    private:
+    protected:
         int health;
         int damage;
     public:
-        enemy();
+        enemy(int, int, int, int);
         void taken(mc* player);
         void dealt(mc* player);
         bool ifDefeated();
         void showStatus();
-}
+};
+
+#endif
